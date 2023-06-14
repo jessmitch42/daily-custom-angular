44a85f9072a4694d1c3bccfd6cd5b7420c902bcb
Jess Mitchell <jess@jessmitchell.ca>
2023-06-14 14:39:51 -0400

    add basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoinFormComponent } from './join-form/join-form.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, JoinFormComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/join-form/join-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-form',
+  templateUrl: './join-form.component.html',
+  styleUrls: ['./join-form.component.css']
+})
+export class JoinFormComponent {
+
+}
