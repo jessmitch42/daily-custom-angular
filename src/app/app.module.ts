a86c1732988be7db12d79bb640f242b2dba87548
Jess Mitchell <jess@jessmitchell.ca>
2023-06-21 14:17:42 -0400

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JoinFormComponent } from './join-form/join-form.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, JoinFormComponent],
+  declarations: [AppComponent, JoinFormComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
